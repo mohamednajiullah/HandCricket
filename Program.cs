8fcb595e63ac9d0441522be06f97dcad05f602fe
Kartik Mahendra <36449866+kmahendra-v1@users.noreply.github.com>
2020-01-30 11:51:48 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HandCricket
+{
+    class Program
+    {
+        static void Main()
+        {
+            Player playerA = new Player("A");
+            Player playerB = new Player("B");
+            GameSimulator.SimulateGame(playerA, playerB);
+            Console.Read();
+        }
+    }
+}
